a9194114716cd2eb7759f44bf8fc41f4e6a49612
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2020-09-04 18:17:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-import setuptools
+from setuptools import setup
+
+setup(
+   name='pydistil',
+   version='1.0',
+   description='Module to distil the python scripts',
+   author='Amol Deshmukh',
+   author_email='amoldeshmukh137@gmail.com',
+   packages=['pydistil'],  
+   install_requires=['--', '--'], #external packages as dependencies
+)
