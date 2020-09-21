966d4d73a6358ef86d888771fcf12bbe7f437d07
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2020-09-21 12:18:07 -0400

    Update src.py

--- src.py
+++ src.py
@@ -2,12 +2,15 @@ import os
 import sys
 
 def count_py_files(dirname):
-  if os.path.exists(os.path.join(dirname, '.git')):
-    count = 0
-    for root, dirs, files in os.walk(dirname):
-      count += len([f for f in files if f.endswith('.py')])
-    print(f'{dirname} has {count} Python files')
-  for name in os.listdir(dirname):
-    path = os.path.join(dirname, name):
-      if os.path.isdir(path):
-        count_py_files(path)
+    if os.path.exists(os.path.join(dirname, '.git')):
+        count = 0
+        for root, dirs, files in os.walk(dirname):
+            count += len([f for f in files if f.endswith('.py')])
+        print(f'{dirname} has {count} Python files')
+    for name in os.listdir(dirname):
+        path = os.path.join(dirname, name)
+        if os.path.isdir(path):
+            count_py_files(path)
+
+if __name__ == '__main__':
+    count_py_files('./github')
