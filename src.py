8411053e533302b398a93bcccb0bc552990f0584
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2020-09-27 00:34:42 -0400

    Update src.py

--- src.py
+++ src.py
@@ -12,5 +12,22 @@ def count_py_files(dirname):
         if os.path.isdir(path):
             count_py_files(path)
 
+def distill_py_file(orig='orig.py', dest='dest.py'):
+    with open(orig, 'r') as file:
+        lines = file.readlines()    
+    lines = [line for line in lines if line!='\n']
+    lines = [line for line in lines if not line.strip().startswith('#')]
+    lines = [line for line in lines if len(line.split('"""'))!=3]    
+    output, count = [], 0
+    for line in lines:
+        if line.strip()[:3] == '"""':
+            count += 1
+        else: 
+            if count%2 == 0: 
+                output.append(line)    
+    with open(dest, 'w') as file:
+        for line in output:
+            file.write(line)            
+            
 if __name__ == '__main__':
     count_py_files('./github')
